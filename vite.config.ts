c9afda1ce1109ffb684f0a2b61516ee9a40bad06
Raphael Nogueira <raphael_dc1nogueira@hotmail.com.br>
2025-10-16 17:55:20 -0300

    ajuste

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Allow overriding base via env (useful for CI deploys).
-  base: process.env.VITE_BASE || '/Discovery.Entretenimento.Portal/',
+  base: '/',
   css: {
     postcss: './postcss.config.js',
   },
